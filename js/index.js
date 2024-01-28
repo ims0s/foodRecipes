117574ef63d16a75051c6336fcbe07ac12c7f959
ims0s <sorour2001@gmail.com>
2024-01-28 04:20:30 +0300

    sideBar done

--- js/index.js
+++ js/index.js
@@ -22,3 +22,44 @@ fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=')
     meals=document.querySelectorAll('.meal')
     mealDetails(meals,foodsContainer,loader)
 })
+
+function openSideNav() {
+    $(".side-nav-menu").animate({
+        left: 0
+    }, 500)
+
+
+    $(".open-close-icon").removeClass("fa-align-justify");
+    $(".open-close-icon").addClass("fa-x");
+
+
+    for (let i = 0; i < 5; i++) {
+        $(".links li").eq(i).animate({
+            top: 0
+        }, (i + 5) * 100)
+    }
+}
+
+function closeSideNav() {
+    let boxWidth = $(".side-nav-menu .nav-tab").outerWidth()
+    $(".side-nav-menu").animate({
+        left: -boxWidth
+    }, 500)
+
+    $(".open-close-icon").addClass("fa-align-justify");
+    $(".open-close-icon").removeClass("fa-x");
+
+
+    $(".links li").animate({
+        top: 300
+    }, 500)
+}
+
+closeSideNav()
+$(".side-nav-menu i.open-close-icon").click(() => {
+    if ($(".side-nav-menu").css("left") == "0px") {
+        closeSideNav()
+    } else {
+        openSideNav()
+    }
+})
